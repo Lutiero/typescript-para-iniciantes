d113bfb8635e002408e821fc6853984b29f670d5
Lutiero <3720@ucdb.br>
2023-11-01 13:54:53 -0400

    tuples

--- script.js
+++ script.js
@@ -1,53 +1,65 @@
-// 1 - Crie uma interface UserData para o formulário abaixo
-// 2 - Crie uma variável global UserData no window, ela será um objeto qualquer
-// 3 - Adicione um evento de keyup ao formulário
-// 4 - Quando o evento ocorrer adicione a {[id]: value} ao UserData
-// 5 - Salve UserData no localStorage
-// 6 - Crie uma User Type Guard, para verificar se o valor de localStorage é compatível com UserData
-// 7 - Ao refresh da página, preencha os valores de localStorage (caso seja UserData) no formulário e em window.UserData
-window.UserData = {};
-function isUserData(obj) {
-    if (obj && typeof obj === "object") {
-        return true;
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
-    else {
-        return false;
-    }
+};
+function fetchVendas() {
+    return __awaiter(this, void 0, void 0, function () {
+        var response, data;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0: return [4 /*yield*/, fetch("https://api.origamid.dev/json/vendas.json")];
+                case 1:
+                    response = _a.sent();
+                    return [4 /*yield*/, response.json()];
+                case 2:
+                    data = _a.sent();
+                    somarVendas(data);
+                    return [2 /*return*/];
+            }
+        });
+    });
 }
-function validJSON(str) {
-    try {
-        JSON.parse(str);
+fetchVendas();
+function somarVendas(vendas) {
+    var totalFor = 0;
+    for (var index = 0; index < vendas.length; index++) {
+        totalFor += vendas[index][1];
     }
-    catch (error) {
-        return false;
-    }
-    return true;
+    document.body.innerHTML += "<p>Total com For loop: R$ ".concat(totalFor, "</p>");
+    var totalReduce = vendas.reduce(function (anterior, atual) {
+        return anterior + atual[1];
+    }, 0);
+    document.body.innerHTML += "<p>Total com reduce: R$ ".concat(totalReduce, "</p>");
 }
-function loadLocalStorage() {
-    var loadUserData = localStorage.getItem("UserData");
-    if (loadUserData && validJSON(loadUserData)) {
-        var UserData = JSON.parse(loadUserData);
-        if (isUserData(UserData)) {
-            Object.entries(UserData).forEach(function (_a) {
-                var key = _a[0], value = _a[1];
-                var input = document.getElementById(key);
-                if (input instanceof HTMLInputElement) {
-                    input.value = value;
-                    window.UserData[key] = value;
-                }
-            });
-        }
-    }
-}
-loadLocalStorage();
-function handleInput(_a) {
-    var target = _a.target;
-    if (target instanceof HTMLInputElement) {
-        if (isUserData(target)) {
-            window.UserData[target.id] = target.value;
-            localStorage.setItem("UserData", JSON.stringify(window.UserData));
-        }
-    }
-}
-var form = document.querySelector("#form");
-form === null || form === void 0 ? void 0 : form.addEventListener("keyup", handleInput);
--- script.ts
+++ script.ts
@@ -1,67 +1,26 @@
-// 1 - Crie uma interface UserData para o formulário abaixo
-// 2 - Crie uma variável global UserData no window, ela será um objeto qualquer
-// 3 - Adicione um evento de keyup ao formulário
-// 4 - Quando o evento ocorrer adicione a {[id]: value} ao UserData
-// 5 - Salve UserData no localStorage
-// 6 - Crie uma User Type Guard, para verificar se o valor de localStorage é compatível com UserData
-// 7 - Ao refresh da página, preencha os valores de localStorage (caso seja UserData) no formulário e em window.UserData
-
-interface UserData {
-  nome?: string;
-  email?: string;
-  cpf?: string;
+async function fetchVendas() {
+  const response = await fetch("https://api.origamid.dev/json/vendas.json");
+  const data = await response.json();
+  somarVendas(data);
 }
 
-interface Window {
-  //Ponta de falha
-  UserData: any;
+fetchVendas();
+
+type Venda = [string, number, string, ProdutoDetalhes];
+interface ProdutoDetalhes {
+  marca: string;
+  cor: string;
 }
 
-window.UserData = {};
-
-function isUserData(obj: unknown): obj is UserData {
-  if (obj && typeof obj === "object") {
-    return true;
-  } else {
-    return false;
+function somarVendas(vendas: Venda[]) {
+  let totalFor = 0;
+  for (let index = 0; index < vendas.length; index++) {
+    totalFor += vendas[index][1];
   }
+  document.body.innerHTML += `<p>Total com For loop: R$ ${totalFor}</p>`;
+  const totalReduce = vendas.reduce((anterior, atual) => {
+    return anterior + atual[1];
+  }, 0);
+
+  document.body.innerHTML += `<p>Total com reduce: R$ ${totalReduce}</p>`;
 }
-
-function validJSON(str: string) {
-  try {
-    JSON.parse(str);
-  } catch (error) {
-    return false;
-  }
-  return true;
-}
-
-function loadLocalStorage() {
-  const loadUserData = localStorage.getItem("UserData");
-  if (loadUserData && validJSON(loadUserData)) {
-    const UserData = JSON.parse(loadUserData);
-    if (isUserData(UserData)) {
-      Object.entries(UserData).forEach(([key, value]) => {
-        const input = document.getElementById(key);
-        if (input instanceof HTMLInputElement) {
-          input.value = value;
-          window.UserData[key] = value;
-        }
-      });
-    }
-  }
-}
-
-loadLocalStorage();
-
-function handleInput({ target }: KeyboardEvent) {
-  if (target instanceof HTMLInputElement) {
-    if (isUserData(target)) {
-      window.UserData[target.id] = target.value;
-      localStorage.setItem("UserData", JSON.stringify(window.UserData));
-    }
-  }
-}
-
-const form = document.querySelector<HTMLElement>("#form");
-form?.addEventListener("keyup", handleInput);
